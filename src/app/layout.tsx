6335d33eac2d578f023fcbbff644b00a6f407e53
PangHae <yds05074@gmail.com>
2025-12-05 09:21:06 +0900

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const pretendard = localFont({
+	src: './fonts/PretendardVariable.woff2',
+	display: 'swap',
+	weight: '45 920',
+	variable: '--font-pretendard',
+});
+
+export const metadata: Metadata = {
+  title: "TEAM NORU",
+  description: "NORU NORU",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${pretendard.className} antialiased flex w-dvw h-dvh overflow-hidden bg-custom-gray-bg`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
